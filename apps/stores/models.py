9b4eeceb6c62fe4f833907ded7b5d9367a40037a
Akimana AJA <akimanaja@Jeans-MBP.local>
2020-03-06 17:47:24 +0200

    Initiate monthly aggregated data retrieval

--- apps/stores/models.py
+++ apps/stores/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import Q
+from django.utils.timezone import now
 
 from d2dstore.models import BaseModel
 from app_utils.constants import STORE_CHOICES
@@ -15,9 +16,11 @@ class Store(BaseModel):
     is_property = models.BooleanField(default=False)
     is_inflow = models.BooleanField(default=False)
     description = models.CharField(max_length=200, default='*Home expense')
+    action_date = models.DateField(blank=False, null=False, default=now)
     user = models.ForeignKey(User, related_name='stores', on_delete=models.CASCADE)
 
     def get_total(self, store_type='use', is_inflow=False):
         store_filter = (Q(record_type=store_type, is_inflow=is_inflow))
         total = sum([store.amount for store in self.objects.filter(store_filter)])
         return total
+
--- apps/stores/schema/queries.py
+++ apps/stores/schema/queries.py
@@ -1,6 +1,7 @@
 import graphene
+from graphene import relay
 from graphene_django import DjangoObjectType
-from django.db.models import Q
+from django.db.models import Q, Count
 from graphql_jwt.decorators import login_required
 
 from apps.stores.models import Store
@@ -11,10 +12,22 @@ class StoreType(DjangoObjectType):
         model = Store
 
 
+class MonthType(DjangoObjectType):
+    class Meta:
+        model = Store
+        fields = {
+            'action_date': ['exact', 'action_date__month'],
+            'action_date': ['exact', 'action_date__year']
+        }
+        interfaces = (relay.Node,)
+
+
 class StoreQuery(graphene.AbstractType):
     stores = graphene.List(StoreType, search=graphene.String(), store_type=graphene.String())
     total_inflow = graphene.Int(store_type=graphene.String())
     total_outflow = graphene.Int(store_type=graphene.String())
+    store_count = graphene.Int()
+    monthly_store = graphene.List(MonthType)
 
     @login_required
     def resolve_stores(self, info, search=None, store_type='use', **kwargs):
@@ -33,3 +46,16 @@ class StoreQuery(graphene.AbstractType):
     @login_required
     def resolve_total_outflow(self, info, store_type='use', **kwargs):
         return Store.get_total(Store, store_type=store_type, is_inflow=False)
+
+    @login_required
+    def resolve_store_count(self):
+        return Store.objects.count()
+
+    @login_required
+    def resolve_monthly_store(self):
+        monthly_stores = Store.objects \
+            .values() \
+            .annotate(count=Count('pk'))
+        import pdb
+        pdb.set_trace()
+        return monthly_stores

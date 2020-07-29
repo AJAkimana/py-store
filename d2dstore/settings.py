d45b5ac43c3d681806e59b8e3498e9dd04e0952d
Akimana Jean d Amour <akimanaja17@gmail.com>
2020-07-29 21:53:42 +0200

    REfactor setting

--- d2dstore/settings.py
+++ d2dstore/settings.py
@@ -25,8 +25,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 DEBUG = os.getenv("DEBUG", False)
 ALLOWED_HOSTS = ['*']
 
--- manage.py
+++ manage.py
@@ -9,8 +9,7 @@ def main():
     try:
         from django.core.management import execute_from_command_line
         from django.core.management.commands.runserver import Command as runserver
-        runserver.default_addr = "0.0.0.0"
-        runserver.default_port = "8000"
+        runserver.default_port = os.getenv('PORT', 8000)
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "

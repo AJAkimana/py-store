698339986342cfb364e989403eada24cc1ed204d
Akimana Jean d Amour <akimanaja17@gmail.com>
2020-08-19 20:07:25 +0200

    Create a mutatuion to store multiple stores

--- app_utils/model_types/store.py
+++ app_utils/model_types/store.py
@@ -1,4 +1,4 @@
-from graphene import ObjectType, List, Int, String, Float
+import graphene
 from graphene_django import DjangoObjectType
 from apps.stores.models import Store
 
@@ -8,18 +8,27 @@ class StoreType(DjangoObjectType):
 		model = Store
 
 
-class StorePaginatorType(ObjectType):
-	page_data = List(StoreType)
-	num_pages = Int()
-	total_count = Int()
+class StoreInputType(graphene.InputObjectType):
+	amount = graphene.Float()
+	record_type = graphene.String()
+	is_property = graphene.Boolean()
+	is_inflow = graphene.Boolean()
+	action_date = graphene.Date()
+	description = graphene.String()
 
 
-class MonthType(ObjectType):
-	label = String()
-	value = Int()
+class StorePaginatorType(graphene.ObjectType):
+	page_data = graphene.List(StoreType)
+	num_pages = graphene.Int()
+	total_count = graphene.Int()
 
 
-class StoreRatioType(ObjectType):
-	inflow = Int()
-	outflow = Int()
-	percent = Float()
+class MonthType(graphene.ObjectType):
+	label = graphene.String()
+	value = graphene.Int()
+
+
+class StoreRatioType(graphene.ObjectType):
+	inflow = graphene.Int()
+	outflow = graphene.Int()
+	percent = graphene.Float()
--- apps/stores/schema/mutations.py
+++ apps/stores/schema/mutations.py
@@ -4,31 +4,42 @@ from graphql_jwt.decorators import login_required
 
 from app_utils.constants import STORE_CHOICES
 from apps.stores.models import Store
-from app_utils.model_types.store import StoreType
+from app_utils.model_types.store import StoreInputType
 
 
 class CreateStore(graphene.Mutation):
-    message = graphene.String()
-    new_store = graphene.Field(StoreType)
-
-    class Arguments:
-        amount = graphene.Float()
-        record_type = graphene.String()
-        is_property = graphene.Boolean()
-        is_inflow = graphene.Boolean()
-        description = graphene.String()
-
-    @login_required
-    def mutate(self, info, **kwargs):
-        kwargs['user_id'] = info.context.user.id
-        store_type = [item for item in STORE_CHOICES if item[0] == kwargs['record_type']]
-        if not store_type:
-            raise GraphQLError('Invalid store type')
-        store = Store(**kwargs)
-        store.save()
-
-        return CreateStore(message="Success", new_store=store)
+	message = graphene.String()
+	total_saved = graphene.Int()
+	total_not_saved = graphene.Int()
+	
+	class Arguments:
+		stores = graphene.List(StoreInputType)
+	
+	@login_required
+	def mutate(self, info, **kwargs):
+		saved = 0
+		not_saved = 0
+		stores = kwargs['stores']
+		for store in stores:
+			store_type = [item for item in STORE_CHOICES if item[0] == store['record_type']]
+			if not store_type:
+				raise GraphQLError('Invalid store type')
+			has_saved = Store.objects.filter(
+				action_date=store['action_date'],
+				description=store['description']).first()
+			if not has_saved:
+				store['user_id'] = info.context.user.id
+				new_store = Store(**store)
+				new_store.save()
+				saved += 1
+			else:
+				not_saved += 1
+		
+		return CreateStore(
+			message="stores successfully saved",
+			total_saved=saved,
+			total_not_saved=not_saved)
 
 
 class StoreMutation(graphene.ObjectType):
-    create_store = CreateStore.Field()
+	create_store = CreateStore.Field()

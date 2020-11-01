2920102a08ce382373f541b993632ba9093dd663
Akimana Jean d Amour <akimanaja17@gmail.com>
2020-11-01 06:31:13 +0200

    Print supplied code

--- apps/stores/views.py
+++ apps/stores/views.py
@@ -53,6 +53,7 @@ class MigrateStoreView(APIView):
 						prop_not_saved += 1
 				saved += 1
 			else:
+				print(store['action_date'])
 				print(get_errors(serializer.errors))
 				not_saved += 1
 		message = f'{saved} stores saved({prop_saved}p), {not_saved} remained({prop_not_saved})'

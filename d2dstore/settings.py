b5c22703bbcef2c3417c30affd046ad788ab1e3b
Akimana A Jean d Amour <akimanaja17@gmail.com>
2022-12-14 19:02:58 +0200

    Change static url link

--- d2dstore/settings.py
+++ d2dstore/settings.py
@@ -183,7 +183,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = '/static/'
+STATIC_URL = '/apis/static/'
 STATICFILES_DIRS = [
   os.getenv('ROOT_STATIC_DI', '')
 ]

9201cb8060528acc21ef3764c7b9f1fd712510f0
Akimana A Jean d Amour <akimanaja17@gmail.com>
2022-12-14 17:47:59 +0200

    Fix env

--- d2dstore/settings.py
+++ d2dstore/settings.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 # from app_utils.helpers import backup_filename
 from app_utils.helpers import backup_filename
 
-load_dotenv(os.path.abspath('.env'))
+load_dotenv(os.path.abspath('app.env'))
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 

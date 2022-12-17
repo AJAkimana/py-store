f82fda1990265a46c79c8a4667cfbe8d44efb62f
Akimana A Jean d Amour <akimanaja17@gmail.com>
2022-12-17 19:27:25 +0200

    Create admin dashboard for property

--- apps/properties/admin.py
+++ apps/properties/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
-from apps.properties.models import Property, PropDetail
+from django.contrib.admin import ModelAdmin as BaseModelAdmin
+from django.utils.translation import gettext_lazy as _
 
-admin.site.register(Property)
-admin.site.register(PropDetail)
+from apps.properties.forms import PropertyChangeForm, PropertyCreationForm, property_fields
+from apps.properties.models import Property
+
+
+class PropertyAdmin(BaseModelAdmin):
+	ordering = ["created_at"]
+	add_form = PropertyCreationForm
+	form = PropertyChangeForm
+	model = Property
+	list_display = ['name', 'price', 'description', 'owner', 'is_active', 'display_detail']
+	list_filter = ['owner', 'is_active']
+	fieldsets = (
+		(_('Property Information'), {'fields': [('name', 'description', 'is_active'), ]}),
+		(_('Amount Information'), {'fields': ('price',)}),
+		(_('Property OwnerShip'), {'fields': ('owner',)}),
+		(_('Important Dates'), {'fields': ('created_at', 'updated_at',)}),
+	)
+	add_fieldsets = (
+		(None, {
+			'classes': ('wide',),
+			'fields': [('name', 'price'), 'description', ('owner', 'is_active')]
+		}),
+	)
+	search_fields = ['price', 'name']
+
+
+admin.site.register(Property, PropertyAdmin)
+# admin.site.register(PropDetail)

9ac64efe4b258cb747707b95c993c94951790988
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-03-23 07:49:45 +0200

    Create store relations with household

--- apps/stores/models.py
+++ apps/stores/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.timezone import now
 
+from apps.households.models import Household
 from apps.properties.models import Property
 from d2dstore.models import BaseModel
 from app_utils.constants import STORE_CHOICES
@@ -8,32 +9,37 @@ from apps.users.models import User
 
 
 class Store(BaseModel):
-	amount = models.FloatField()
-	record_type = models.CharField(
-		max_length=20,
-		choices=STORE_CHOICES,
-		default='use')
-	is_property = models.BooleanField(default=False)
-	is_inflow = models.BooleanField(default=False)
-	description = models.CharField(max_length=200, default='*Home expense')
-	action_date = models.DateField(blank=False, null=False, default=now)
-	user = models.ForeignKey(User, related_name='stores', on_delete=models.PROTECT)
-	property = models.ForeignKey(Property, related_name='stores', on_delete=models.PROTECT, null=True)
+    amount = models.FloatField()
+    record_type = models.CharField(
+        max_length=20,
+        choices=STORE_CHOICES,
+        default='use')
+    is_property = models.BooleanField(default=False)
+    is_inflow = models.BooleanField(default=False)
+    description = models.CharField(max_length=200, default='*Home expense')
+    action_date = models.DateField(blank=False, null=False, default=now)
+    user = models.ForeignKey(
+        User, related_name='stores', on_delete=models.PROTECT)
+    property = models.ForeignKey(
+        Property, related_name='stores', on_delete=models.PROTECT, null=True)
+    household = models.ForeignKey(
+        Household, related_name='stores', on_delete=models.PROTECT, null=True)
 
-	class Meta:
-		db_table = "stores"
-		ordering = ['-action_date']
-		unique_together = ['description', 'action_date', 'user']
+    class Meta:
+        db_table = "stores"
+        ordering = ['-action_date']
+        unique_together = ['description', 'action_date', 'user']
 
-	def __str__(self):
-		return f"{self.description} Amount: {self.amount}"
+    def __str__(self):
+        return f"{self.description} Amount: {self.amount}"
 
 
 class RecurringStore(BaseModel):
-	name = models.CharField(max_length=200)
-	user = models.ForeignKey(User, related_name='recurring_stores', on_delete=models.PROTECT)
+    name = models.CharField(max_length=200)
+    user = models.ForeignKey(
+        User, related_name='recurring_stores', on_delete=models.PROTECT)
 
-	class Meta:
-		db_table = 'recurring_stores'
-		ordering = ['name']
-		unique_together = ['name', 'user']
+    class Meta:
+        db_table = 'recurring_stores'
+        ordering = ['name']
+        unique_together = ['name', 'user']

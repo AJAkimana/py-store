ea08f181aa5a8119e8f7ad225c1ff81fb2f30e53
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-03-23 07:51:54 +0200

    Add store member access levels

--- app_utils/constants.py
+++ app_utils/constants.py
@@ -1,3 +1,4 @@
 STORE_CHOICES = [('use', 'Store'), ('debt', 'Debt'), ('online', 'Online')]
 AMOUNT_TYPES = [('in', 'Inflow'), ('out', 'Outflow')]
 SALARY_TYPES = [('ns', 'Net Salary'), ('gs', 'Gross Salary')]
+MEMBER_ACCESS_LEVELS = [(1, 'Member Admin'), (2, 'Staff Admin'), (3, 'Member')]

aecaf203f680c34f2fcf492b304c82c0afab658f
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-06-30 15:17:12 +0200

    Tweak migrations

--- apps/properties/models.py
+++ apps/properties/models.py
@@ -6,50 +6,52 @@ from apps.users.models import User
 
 
 class Property(BaseModel):
-    name = models.CharField(blank=False, max_length=50)
-    price = models.FloatField(blank=False)
-    description = models.CharField(max_length=255, default='Asset')
-    cover_image = models.CharField(
-        max_length=255, default='/images/products/product_1.png')
-    owner = models.ForeignKey(
-        User, related_name='properties', on_delete=models.CASCADE)
-    is_active = models.BooleanField(default=True)
-    household = models.ForeignKey(
-        Household, related_name='properties', on_delete=models.PROTECT, null=True)
+	name = models.CharField(blank=False, max_length=50)
+	price = models.FloatField(blank=False)
+	description = models.CharField(max_length=255, default='Asset')
+	cover_image = models.CharField(
+		max_length=255, default='/images/products/product_1.png')
+	owner = models.ForeignKey(
+		User, related_name='properties', on_delete=models.CASCADE)
+	is_active = models.BooleanField(default=True)
+	household = models.ForeignKey(
+		Household, related_name='properties', on_delete=models.PROTECT, null=True)
+	household_1 = models.ForeignKey(
+		Household, related_name='new_properties', on_delete=models.PROTECT, null=True)
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
-    class Meta:
-        db_table = "properties"
-        ordering = ['name']
-        unique_together = ['name', 'owner']
-        verbose_name_plural = "Properties"
+	class Meta:
+		db_table = "properties"
+		ordering = ['name']
+		unique_together = ['name', 'owner']
+		verbose_name_plural = "Properties"
 
-    def display_detail(self):
-        """Create a string for the property details. This is required to be displayed in Admin"""
-        inflow = sum(
-            [p_detail.amount for p_detail in self.prop_details.filter(type='in')])
-        outflow = sum(
-            [p_detail.amount for p_detail in self.prop_details.filter(type='out')])
+	def display_detail(self):
+		"""Create a string for the property details. This is required to be displayed in Admin"""
+		inflow = sum(
+			[p_detail.amount for p_detail in self.prop_details.filter(type='in')])
+		outflow = sum(
+			[p_detail.amount for p_detail in self.prop_details.filter(type='out')])
 
-        return f"{inflow if inflow > 0 else '-'}/{outflow if outflow > 0 else '-'}"
+		return f"{inflow if inflow > 0 else '-'}/{outflow if outflow > 0 else '-'}"
 
-    display_detail.short_description = 'Transactions(IN/OUT)'
+	display_detail.short_description = 'Transactions(IN/OUT)'
 
 
 class PropDetail(BaseModel):
-    title = models.CharField(max_length=255, blank=False)
-    amount = models.FloatField(blank=False)
-    type = models.CharField(choices=AMOUNT_TYPES,
-                            default='out', blank=False, max_length=10)
-    property = models.ForeignKey(
-        Property, related_name='prop_details', on_delete=models.PROTECT, null=True)
+	title = models.CharField(max_length=255, blank=False)
+	amount = models.FloatField(blank=False)
+	type = models.CharField(choices=AMOUNT_TYPES,
+													default='out', blank=False, max_length=10)
+	property = models.ForeignKey(
+		Property, related_name='prop_details', on_delete=models.PROTECT, null=True)
 
-    def __str__(self):
-        return self.title
+	def __str__(self):
+		return self.title
 
-    class Meta:
-        ordering = ['title']
-        db_table = "property_details"
-        verbose_name_plural = "Property Details"
+	class Meta:
+		ordering = ['title']
+		db_table = "property_details"
+		verbose_name_plural = "Property Details"
--- apps/stores/models.py
+++ apps/stores/models.py
@@ -9,37 +9,40 @@ from apps.users.models import User
 
 
 class Store(BaseModel):
-    amount = models.FloatField()
-    record_type = models.CharField(
-        max_length=20,
-        choices=STORE_CHOICES,
-        default='use')
-    is_property = models.BooleanField(default=False)
-    is_inflow = models.BooleanField(default=False)
-    description = models.CharField(max_length=200, default='*Home expense')
-    action_date = models.DateField(blank=False, null=False, default=now)
-    user = models.ForeignKey(
-        User, related_name='stores', on_delete=models.PROTECT)
-    property = models.ForeignKey(
-        Property, related_name='stores', on_delete=models.PROTECT, null=True)
-    household = models.ForeignKey(
-        Household, related_name='stores', on_delete=models.PROTECT, null=True)
+	amount = models.FloatField()
+	record_type = models.CharField(
+		max_length=20,
+		choices=STORE_CHOICES,
+		default='use'
+	)
+	is_property = models.BooleanField(default=False)
+	is_inflow = models.BooleanField(default=False)
+	description = models.CharField(max_length=200, default='*Home expense')
+	action_date = models.DateField(blank=False, null=False, default=now)
+	user = models.ForeignKey(
+		User, related_name='stores', on_delete=models.PROTECT)
+	property = models.ForeignKey(
+		Property, related_name='stores', on_delete=models.PROTECT, null=True)
+	household = models.ForeignKey(
+		Household, related_name='stores', on_delete=models.PROTECT, null=True)
+	household_1 = models.ForeignKey(
+		Household, related_name='prop_stores', on_delete=models.PROTECT, null=True)
 
-    class Meta:
-        db_table = "stores"
-        ordering = ['-action_date']
-        unique_together = ['description', 'action_date', 'user']
+	class Meta:
+		db_table = "stores"
+		ordering = ['-action_date']
+		unique_together = ['description', 'action_date', 'user']
 
-    def __str__(self):
-        return f"{self.description} Amount: {self.amount}"
+	def __str__(self):
+		return f"{self.description} Amount: {self.amount}"
 
 
 class RecurringStore(BaseModel):
-    name = models.CharField(max_length=200)
-    user = models.ForeignKey(
-        User, related_name='recurring_stores', on_delete=models.PROTECT)
+	name = models.CharField(max_length=200)
+	user = models.ForeignKey(
+		User, related_name='recurring_stores', on_delete=models.PROTECT)
 
-    class Meta:
-        db_table = 'recurring_stores'
-        ordering = ['name']
-        unique_together = ['name', 'user']
+	class Meta:
+		db_table = 'recurring_stores'
+		ordering = ['name']
+		unique_together = ['name', 'user']

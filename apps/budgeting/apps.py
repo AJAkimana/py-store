42c08d4a07192259c0af921386c1234cdd01cf60
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-03-17 09:16:42 +0200

    Create migrations

--- apps/budgeting/apps.py
+++ apps/budgeting/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BudgetingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'budgeting'
+    name = 'apps.budgeting'
--- apps/budgeting/models.py
+++ apps/budgeting/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils.timezone import now
 
 from app_utils.constants import BUDGET_STATUSES
+from apps.households.models import Household
+from apps.users.models import User
 from d2dstore.models import BaseModel
 
 
@@ -14,8 +16,35 @@ class Budget(BaseModel):
 	)
 	start_date = models.DateField(blank=False, null=False, default=now)
 	end_date = models.DateField(blank=False, null=False, default=now)
+	user = models.ForeignKey(
+		User,
+		related_name='budgets',
+		on_delete=models.PROTECT,
+		null=True
+	)
+	household = models.ForeignKey(
+		Household,
+		related_name='budgets',
+		on_delete=models.PROTECT,
+		null=True
+	)
+
+	class Meta:
+		db_table = "budgets"
+		ordering = ['-start_date']
+		unique_together = ['name', 'user']
 
 
 class BudgetItem(BaseModel):
 	name = models.CharField(blank=False, max_length=50)
 	amount = models.FloatField()
+	budget = models.ForeignKey(
+		Budget,
+		related_name='budgets',
+		on_delete=models.PROTECT
+	)
+
+	class Meta:
+		db_table = "budget_items"
+		ordering = ['name']
+		unique_together = ['name', 'amount']

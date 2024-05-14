79035609ddd344b09f3327976a1ab836a16b08ee
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-05-14 16:39:26 +0200

    Create admin dashboard for the budgeting

--- apps/budgeting/admin.py
+++ apps/budgeting/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin as BaseModelAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from apps.budgeting.forms import BudgetChangeForm, BudgetCreationForm, BudgetItemCreationForm, BudgetItemChangeForm
+from apps.budgeting.models import Budget, BudgetItem
+
+
+class BudgetAdmin(BaseModelAdmin):
+	ordering = ["created_at"]
+	add_form = BudgetCreationForm
+	form = BudgetChangeForm
+	model = Budget
+	list_display = ['name', 'description', 'status', 'start_date', 'end_date']
+	list_filter = ['name', 'status', 'start_date', 'end_date']
+	fieldsets = (
+		(_('Detailed Information'), {'fields': ('name', 'description', 'status',)}),
+		(_('Important Dates'), {'fields': ('start_date', 'end_date',)}),
+	)
+	add_fieldsets = (
+		(None, {
+			'classes': ('wide',),
+			'fields': ['name', 'description', 'status', 'start_date', 'end_date']
+		}),
+	)
+	search_fields = ['name', 'status']
+
+
+class BudgetItemAdmin(BaseModelAdmin):
+	ordering = ["created_at"]
+	add_form = BudgetItemCreationForm
+	form = BudgetItemChangeForm
+	model = BudgetItem
+	list_display = ['name', 'amount']
+	list_filter = ['name']
+	fieldsets = (
+		(_('Detailed Information'), {'fields': ('name', 'amount',)}),
+	)
+	add_fieldsets = (
+		(None, {
+			'classes': ('wide',),
+			'fields': ['name', 'amount']
+		}),
+	)
+	search_fields = ['name']
+
+
+admin.site.register(Budget, BudgetAdmin)
+admin.site.register(BudgetItem, BudgetItemAdmin)
---
+++ apps/budgeting/forms.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.forms import (
+	UserChangeForm as BaseUserChangeForm,
+	UserCreationForm as BaseUserCreationForm)
+from apps.budgeting.models import Budget, BudgetItem
+
+
+class BudgetForm:
+	class Meta:
+		model = Budget
+		fields = ['name', 'description', 'status', 'start_date', 'end_date']
+		error_class = "error"
+
+
+class BudgetItemForm:
+	class Meta:
+		model = BudgetItem
+		fields = ['name', 'amount']
+		error_class = "error"
+
+
+class BudgetCreationForm(BaseUserCreationForm, BudgetForm):
+	pass
+
+
+class BudgetChangeForm(BaseUserChangeForm, BudgetForm):
+	pass
+
+
+class BudgetItemCreationForm(BaseUserCreationForm, BudgetItemForm):
+	pass
+
+
+class BudgetItemChangeForm(BaseUserChangeForm, BudgetItemForm):
+	pass

ad7eee775533700bab65b6d445bb409cc2b26fd8
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-08-25 16:52:31 +0200

    Update usersetting model to accomodate helper texts

--- app_utils/model_types/user.py
+++ app_utils/model_types/user.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
-from apps.users.models import User, UserSettings
+from apps.users.models import User, UserSettings, Currency
 
 
 class UserType(DjangoObjectType):
@@ -25,3 +25,25 @@ class PaginatorUserType(graphene.ObjectType):
 class UserSettingsType(DjangoObjectType):
 	class Meta:
 		model = UserSettings
+
+	email_notifications_help_text = graphene.String()
+	push_notifications_help_text = graphene.String()
+	budget_alerts_enabled_help_text = graphene.String()
+	budget_alert_threshold_help_text = graphene.String()
+
+	def resolve_email_notifications_help_text(self, info):
+		return UserSettings._meta.get_field("email_notifications").help_text
+
+	def resolve_push_notifications_help_text(self, info):
+		return UserSettings._meta.get_field("push_notifications").help_text
+
+	def resolve_budget_alerts_enabled_help_text(self, info):
+		return UserSettings._meta.get_field("budget_alerts_enabled").help_text
+
+	def resolve_budget_alert_threshold_help_text(self, info):
+		return UserSettings._meta.get_field("budget_alert_threshold").help_text
+
+
+class CurrencyType(DjangoObjectType):
+		class Meta:
+			model = Currency
